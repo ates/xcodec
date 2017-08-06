ead23ef2222e284874b3464ccd8ca3453773e5cd
Artem Teslenko <ates@ipv6.dp.ua>
2017-08-06 20:23:32 +0300

    Fix MTP3 specs

--- include/mtp3.hrl
+++ include/mtp3.hrl
@@ -1,9 +1,9 @@
 -record(mtp3, {
-    opc = 0        :: non_neg_integer(),
-    dpc = 0        :: non_neg_integer(),
-    ni = 0         :: byte(),
+    opc = 0        :: 0..16383, %% ITU OPC/DPC 14 bits
+    dpc = 0        :: 0..16383,
+    ni = 0         :: 0..3,
     si = 0         :: 0..10,
     sls = 0        :: 0..15,
-    priority = 0   :: byte(),
+    priority = 0   :: 0..3,
     payload = <<>> :: binary() 
 }).
